a603c1dd91893cd10db093382571e66023e3dd8c
roc <mario12577@hotmail.com>
2021-06-13 17:18:56 +0700

    no message

---
+++ ข้อ3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0;i>=0 ; i++)
+            {
+                int y = int.Parse(Console.ReadLine());
+                string end = Console.ReadLine();
+
+                if (end == "end")
+                {
+                    End();
+                }
+               
+                    
+            }
+
+        }
+        static void End()
+        {
+           
+            String maxminmeean = Console.ReadLine();
+            if (maxminmeean == "FindMax")
+            {
+                Console.WriteLine("Max is :");
+
+
+
+            }
+            else if (maxminmeean == "FindMin")
+            {
+                Console.WriteLine("Min is :");
+            }
+            else if (maxminmeean == "FindMean")
+            {
+                Console.WriteLine("Mean is :");
+                
+            }
+            else
+            {
+                Console.WriteLine("invalid mode");
+            }
+        }
+
+    }
+}
